22270ce646f10d42b8b06fabd699477dd9ce8b26
JuneCeaser <it22575876@my.sliit.lk>
2026-01-10 19:24:13 +0530

    dash

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 // ⚠️ CHECK PORT
-const API_URL = 'http://localhost:5000/api/places';
+const API_URL = 'http://localhost:5000/api/places';                                                       
 
 export default function Dashboard() {
   const [places, setPlaces] = useState([]);
